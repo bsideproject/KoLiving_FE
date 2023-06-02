9aca381929a45565fc639968cb2b3e18eeb08301
이영우 <zestlee1106@gmail.com>
2023-06-02 23:19:46 +0900

    [feat] storyboook 추가

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,38 +1,44 @@
 import type { StorybookConfig } from '@storybook/nextjs';
+const path = require('path');
+
 const config: StorybookConfig = {
   stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  staticDirs: ['../public'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
     {
-      /**
-       * Fix Storybook issue with PostCSS@8
-       * @see https://github.com/storybookjs/storybook/issues/12668#issuecomment-773958085
-       */
-      //postcss를 활용할 수 있도록 이 부분 추가.
-      name: '@storybook/addon-postcss',
+      name: '@storybook/addon-styling',
       options: {
-        postcssLoaderOptions: {
-          implementation: require('postcss'),
-        },
+        // Check out https://github.com/storybookjs/addon-styling/blob/main/docs/api.md
+        // For more details on this addon's options.
+        postCss: true,
       },
     },
-    '@storybook/addon-mdx-gfm',
   ],
   framework: {
     name: '@storybook/nextjs',
-    options: {},
+    options: {
+      nextConfigPath: path.resolve(__dirname, '../next.config.js'),
+    },
   },
+  babel: async (options) => ({
+    ...options,
+    presets: [
+      [
+        '@babel/preset-react',
+        {
+          runtime: 'automatic',
+        },
+        'preset-react-jsx-transform', // Can name this anything, just an arbitrary alias to avoid duplicate presets'
+      ],
+    ],
+  }),
   docs: {
     autodocs: 'tag',
   },
   webpackFinal: async (config) => {
-    config.module?.rules?.push({
-      test: /\.scss$/,
-      use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
-    });
-
     return config;
   },
 };
