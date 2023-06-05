d6ffa6ac14065db969d40d407e27c0a221a3ba14
이영우 <zestlee1106@gmail.com>
2023-06-05 20:51:42 +0900

    [feat] 포탈에 추가하기 위한 ModalContainer 및 document 추가

---
+++ components/Modal/ModalContainer.tsx
@@ -0,0 +1,14 @@
+import React, { ReactNode } from 'react';
+import { createPortal } from 'react-dom';
+
+interface ModalContainerProps {
+  children: ReactNode;
+}
+
+function ModalContainer({ children }: ModalContainerProps) {
+  const modalRoot = document.getElementById('modal-root');
+  if (!modalRoot) return null;
+  return createPortal(children, modalRoot);
+}
+
+export default ModalContainer;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,6 @@ import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
 import { appWithTranslation } from 'next-i18next';
 import Head from 'next/head';
-import Modal from '../components/Modal/Modal.tsx';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -13,8 +12,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <title>KoLiving</title>
         <link rel="icon" href="/favicon.png" />
       </Head>
-      <Component {...pageProps} />;
-      <Modal />
+      <Component {...pageProps} />
     </>
   );
 }
