f7b85066b2d8a3cb07b960a7e8c52e2018d7337e
JAEMOON <jaemoonzzang@naver.com>
2023-06-06 22:54:56 +0900

    [feat] Button Component tailwindcss 수정

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -5,29 +5,35 @@ interface ButtonProps {
   children: ReactNode;
   onClick: () => void;
   disabled?: boolean;
-  className?: string;
+  size?: 'lg' | 'md' | 'sm';
   type?: 'button' | 'reset' | 'submit';
+  color?: 'r1' | 'r4' | 'g2' | 'none';
 }
 
 /**
- *
  * @param
  * @returns Button Component
  */
-function Button({ type = 'button', children, onClick, disabled, className }: ButtonProps) {
-  const buttonClass = `${styles.button} ${className}`;
-
+function Button({ type = 'button', size, children, onClick, disabled, color }: ButtonProps) {
   return (
-    <button className={styles.button} onClick={onClick} disabled={disabled} type={type}>
+    <button
+      className={`${styles.button} ${styles.size} ${styles.color}`}
+      onClick={onClick}
+      disabled={disabled}
+      type={type}
+      data-size={size}
+      color={color}
+    >
       {children}
     </button>
   );
-};
+}
 
 Button.defaultProps = {
   disabled: false,
-  className: '',
   type: 'button',
+  size: 'md',
+  color: 'r1',
 };
 
 export default Button;
