d9dd28c32cb2408d477e44a062e8d8f11f989417
이영우 <zestlee1106@gmail.com>
2023-06-18 21:20:52 +0900

    [feat] 로그인 페이지 리셋 (로딩 스피너 따로 만들자)

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,24 +1,7 @@
-'use client';
-
 import React from 'react';
-import Lottie from 'react-lottie';
-import * as animationData from '@/public/json/loading.json';
 
 function Login() {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData,
-    rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice',
-    },
-  };
-
-  return (
-    <div>
-      <Lottie options={defaultOptions} height={400} width={400} />
-    </div>
-  );
+  return <div>login</div>;
 }
 
 export default Login;
