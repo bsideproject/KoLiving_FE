d4c57b847f0320dae3ab6bd217ef2d0ca510db80
JAEMOON <jaemoonzzang@naver.com>
2023-07-02 23:52:19 +0900

    [feat] Chip Component 1차 개발

--- components/Chip/Chip.tsx
+++ components/Chip/Chip.tsx
@@ -1,28 +1,39 @@
-import React, { MouseEvent } from 'react';
+import React, { useState, useEffect, useMemo, MouseEvent } from 'react';
 import Close from '@/public/icons/close.svg';
 import Typography from '@/components/Typography/Typography.tsx';
 
 interface ChipProps {
   label: string;
   onDelete?: () => void;
+  clicked: boolean;
 }
 
-/** Node Version 문제로 다시 올려봅니다람쥐... */
-export default function Chip({ label, onDelete }: ChipProps) {
-  const handleDelete = (event: MouseEvent) => {
-    event.preventDefault();
+/** Chip Component 1차 개발 */
+export default function Chip({ label, onDelete, clicked }: ChipProps) {
+  const [isClicked, setIsClicked] = useState(clicked);
+
+  const fillStroke = useMemo(() => {
+    return isClicked ? 'stroke-r1 stroke-[2]' : '';
+  }, [isClicked]);
+
+  useEffect(() => {
+    setIsClicked(isClicked);
+  }, [isClicked]);
+
+  const handleDelete = (e: MouseEvent) => {
+    e.preventDefault();
     if (onDelete) {
       onDelete();
     }
   };
 
   return (
-    <div className="inline-flex items-center bg-r1 bg-opacity-10 text-r1 px-3 py-1 text-sm font-semibold mr-2 mb-2">
+    <div className="inline-flex items-center bg-r1 bg-opacity-10 text-r1 px-3 py-1 text-base font-semibold mr-2 mb-2">
       <Typography variant="label" fontStyle="semiBold" font="pretendard" color="r1">
         {label}
       </Typography>
       {onDelete && (
-        <button className="ml-2 focus:outline-none" onClick={handleDelete} aria-label="Delete">
+        <button className={`"ml-2 focus:outline-none ${fillStroke}`} onClick={handleDelete} aria-label="Delete">
           <Close />
         </button>
       )}
