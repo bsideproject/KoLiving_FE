209f28920eacfa8d0a3376670b679ab5eaab5a02
JAEMOON <jaemoonzzang@naver.com>
2023-07-04 23:36:15 +0900

    [feat] components index.tsx 추가 및 export 로직 수정

---
+++ components/index.tsx
@@ -0,0 +1,17 @@
+export { default as Button } from '@/components/Button/Button.tsx';
+export { default as Checkbox } from '@/components/Checkbox/Checkbox.tsx';
+export { default as Chip } from '@/components/Chip/Chip.tsx';
+export { default as CustomImage } from '@/components/CustomImage/CustomImage.tsx';
+export { default as Header } from '@/components/Header/Header.tsx';
+export { default as Input } from '@/components/Input/Input.tsx';
+export { default as Link } from '@/components/Link/HyperLink.tsx';
+export { default as ModalBox } from '@/components/Modal/ModalBox.tsx';
+export { default as Nav } from '@/components/Nav/Nav.tsx';
+export { default as Radio } from '@/components/Radio/Radio.tsx';
+export { default as Select } from '@/components/Select/Select.tsx';
+export { default as SignUp } from '@/components/SignUp/Step1.tsx';
+export { default as Stepper } from '@/components/Stepper/Stepper.tsx';
+export { default as Textarea } from '@/components/Textarea/Textarea.tsx';
+export { default as Toggle } from '@/components/Toggle/Toggle.tsx';
+export { default as Typography } from '@/components/Typography/Typography.tsx';
+export { default as Space } from '@/components/Space.tsx';
