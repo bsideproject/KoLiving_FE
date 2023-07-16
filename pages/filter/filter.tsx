a8a7e5e39b72ce738037ea8e48b7af4835775c61
JAEMOON <jaemoonzzang@naver.com>
2023-07-17 00:24:04 +0900

    [feat] Filter Component 우선 Commit! Select Component disabled 기능 추가!

--- pages/filter/filter.tsx
+++ pages/filter/filter.tsx
@@ -1,18 +1,11 @@
 import React, { useState, useEffect, useMemo } from 'react';
+import Router from 'next/router';
+import FilterLayout from '@/components/layouts/FilterLayout.tsx';
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { GetStaticPropsContext } from 'next';
-import Typography from '@/components/Typography/Typography.tsx';
-import FilterLayout from '@/components/layouts/FilterLayout.tsx';
-import Select from '@/components/Select/Select.tsx';
-import Input from '@/components/Input/Input.tsx';
+import { Select, Typography, Toggle, Checkbox, Space, Button, Input } from '@/components/index.tsx';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
-import Checkbox from '@/components/Checkbox/Checkbox.tsx';
-import Space from '@/components/Space.tsx';
-import Button from '@/components/Button/Button.tsx';
-import Router from 'next/router';
-import useSignUp from '@/hooks/useSignUp.ts';
-import useModal from '@/hooks/useModal.ts';
 import { guList as GuList, dongList as DongList } from './guDongList.tsx';
 
 export const getStaticProps = async ({ locale }: GetStaticPropsContext) => ({
@@ -59,7 +52,46 @@ export default function Filter() {
           options={filteredDongList}
           register={register('Dong')}
           placeholder={filterTranslation.t('Dong') as string}
+          disabled={!watch('Gu')}
         />
+        <div className="py-[64px] ">
+          <div className="mt-[9px] mb-[20px]">
+            <Typography variant="header" fontStyle="semiBold">
+              {filterTranslation.t('Deposit')}
+            </Typography>
+          </div>
+          <div className="flex justify-between items-center mb-[20px]">
+            <Typography variant="label" fontStyle="semiBold" customClassName="text-[16px]">
+              {filterTranslation.t('View rooms without deposit')}
+            </Typography>
+            <Toggle className="ml-2" />
+          </div>
+          <div className="mb-[8px]">
+            <Typography variant="label" fontStyle="semiBold" customClassName="text-[16px]">
+              {filterTranslation.t('Min 0 ￦ - Max 500,000,000 ￦ ')}
+            </Typography>
+          </div>
+          <div className="mb-[8px]">
+            <Input
+              placeholder={filterTranslation.t('Min') as string}
+              type="text"
+              register={register('min', {
+                validate: (value) => {
+                  return value;
+                },
+              })}
+            />
+          </div>
+          <Input
+            placeholder={filterTranslation.t('Max') as string}
+            type="text"
+            register={register('max', {
+              validate: (value) => {
+                return value;
+              },
+            })}
+          />
+        </div>
         <div className="fixed bottom-0 w-full overflow-x-hidden left-[50%] translate-x-[-50%] px-[20px] max-w-max">
           <div className="w-full">
             <hr className="my-[18px] border-x-0" />
