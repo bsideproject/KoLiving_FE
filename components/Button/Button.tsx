631ee0599ed6b24c45223c2012bdf3dada0c6b2b
JAEMOON <jaemoonzzang@naver.com>
2023-07-18 00:28:05 +0900

    [feat] Filter Page 추가, Button component 다시보기!!

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -5,7 +5,7 @@ interface ButtonProps {
   children: ReactNode;
   onClick?: () => void;
   disabled?: boolean;
-  size?: 'lg' | 'md' | 'sm';
+  size?: 'lg' | 'md' | 'sm' | 'reset' | 'apply';
   type?: 'button' | 'reset' | 'submit';
   color?: 'r1' | 'r4' | 'g2' | 'none' | 'noBg';
 }
@@ -16,14 +16,14 @@ interface ButtonProps {
  */
 function Button({ type = 'button', size, children, onClick, disabled, color }: ButtonProps) {
   const disabledCss = disabled ? styles.disabled : '';
-
+  const styleSize = (size || '').indexOf('px') > -1 ? `w-[${size}]` : styles[`${size}`];
   return (
     <button
-      className={`${styles.button} ${styles[`${size}`]} ${styles[`${color}`]} ${disabledCss}`}
+      className={`${styles.button} ${styleSize} ${styles[`${color}`]} ${disabledCss}`}
       onClick={onClick}
       disabled={disabled}
       type={type}
-      data-size={size}
+      data-size={type !== 'reset' ? size : ''}
       color={color}
     >
       {children}
