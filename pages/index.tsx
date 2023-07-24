d8982c108a9effd4729ca85832b462795a3a5f76
재문 이 <leejaemoon@lotte.net>
2023-07-24 14:20:26 +0900

    [fix] index.tsx, getLayout 오류 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { Component, useEffect, ReactElement, ReactNode } from 'react';
 import type { GetStaticPropsContext } from 'next';
 import 'tailwindcss/tailwind.css';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import RoomCard from '@/components/RoomCard/RoomCard';
 import { fetchRooms } from '@/api/room';
 import { Room } from '@/public/types/room';
-import type { AppProps } from 'next/app';
-import { NextPage, NextPageContext } from 'next';
+import { NextComponentType, NextPage, NextPageContext } from 'next';
 import RoomListLayout from '@/components/layouts/RoomListLayout.tsx';
 import Filter from '@/public/icons/filter.svg';
 import Router, { useRouter, withRouter } from 'next/router';
@@ -17,34 +16,40 @@ export const getStaticProps = async ({ locale }: GetStaticPropsContext) => ({
   },
 });
 
-const Home = () => {
+type HomeProps = NextPage & {
+  getLayout: (page: React.ReactElement, ctx: NextPageContext) => React.ReactNode;
+};
+
+function Home() {
   const [rooms, setRooms] = React.useState<Room[]>([]);
   const router = useRouter();
   const getFilterPage = () => {
     router.push('/room/filter', undefined, { shallow: true });
   };
+
+  const selectRooms = async () => {
+    try {
+      const data = await fetchRooms();
+      setRooms(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // 최초 접근 시 Room 정보 조회
   useEffect(() => {
     (async () => {
-      try {
-        const data = await fetchRooms();
-        setRooms(data);
-      } catch (error) {
-        console.error(error);
-      }
+      await selectRooms();
     })();
   }, []);
 
+  // Filter 변경 시 Room 정보 조회
   useEffect(() => {
     /**
      * @TODO filter 내용에 따라 조회되는 내용 다르게 할 수 있도록 파라미터 정보 보내는 로직 추가 필요
      */
     (async () => {
-      try {
-        const data = await fetchRooms();
-        setRooms(data);
-      } catch (error) {
-        console.error(error);
-      }
+      await selectRooms();
     })();
   }, [router.query]);
 
@@ -56,10 +61,10 @@ const Home = () => {
       ))}
     </div>
   );
-};
+}
 
-export default Home;
-
-(Home as NextPage).getLayout = function getLayout(page: React.ReactElement, ctx: NextPageContext) {
+Home.getLayout = function getLayout(page: React.ReactElement, ctx: NextPageContext) {
   return <RoomListLayout>{page}</RoomListLayout>;
 };
+
+export default Home as HomeProps;
