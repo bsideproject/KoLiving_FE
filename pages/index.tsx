8a099fcc182f2fefb7e2f8a224c45b6bf190ad5d
재문 이 <leejaemoon@lotte.net>
2023-07-24 13:41:37 +0900

    [feat] Main Page RoomList Filter 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,11 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import RoomCard from '@/components/RoomCard/RoomCard';
 import { fetchRooms } from '@/api/room';
 import { Room } from '@/public/types/room';
+import type { AppProps } from 'next/app';
+import { NextPage, NextPageContext } from 'next';
+import RoomListLayout from '@/components/layouts/RoomListLayout.tsx';
+import Filter from '@/public/icons/filter.svg';
+import Router, { useRouter, withRouter } from 'next/router';
 
 export const getStaticProps = async ({ locale }: GetStaticPropsContext) => ({
   props: {
@@ -14,7 +19,10 @@ export const getStaticProps = async ({ locale }: GetStaticPropsContext) => ({
 
 const Home = () => {
   const [rooms, setRooms] = React.useState<Room[]>([]);
-
+  const router = useRouter();
+  const getFilterPage = () => {
+    router.push('/room/filter', undefined, { shallow: true });
+  };
   useEffect(() => {
     (async () => {
       try {
@@ -26,8 +34,23 @@ const Home = () => {
     })();
   }, []);
 
+  useEffect(() => {
+    /**
+     * @TODO filter 내용에 따라 조회되는 내용 다르게 할 수 있도록 파라미터 정보 보내는 로직 추가 필요
+     */
+    (async () => {
+      try {
+        const data = await fetchRooms();
+        setRooms(data);
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+  }, [router.query]);
+
   return (
     <div>
+      <Filter className="stroke-g7 stroke-[2] cursor-pointer" onClick={getFilterPage} />
       {rooms.map((room, idx) => (
         <RoomCard room={room} key={`room-${idx}`} />
       ))}
@@ -36,3 +59,7 @@ const Home = () => {
 };
 
 export default Home;
+
+(Home as NextPage).getLayout = function getLayout(page: React.ReactElement, ctx: NextPageContext) {
+  return <RoomListLayout>{page}</RoomListLayout>;
+};
