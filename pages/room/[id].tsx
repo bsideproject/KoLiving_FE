6b1cb8f0f7b9fc1a46cc915f6693517f10d1b2ce
이영우 <zestlee1106@gmail.com>
2023-07-25 17:28:28 +0900

    [feat] 밑에 태그 추가

--- pages/room/[id].tsx
+++ pages/room/[id].tsx
@@ -2,19 +2,32 @@ import React from 'react';
 import { Header } from '@/components/index.tsx';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
+import { Swiper as SwiperType } from 'swiper/types';
 import styles from './room.module.scss';
 
 export default function Login() {
+  const [currentSlide, setCurrentSlide] = React.useState(0);
+
+  const handleSlideChange = (activeIndex: number) => {
+    setCurrentSlide(activeIndex);
+  };
+
   return (
     <div className="font-pretendard w-full">
-      <Swiper effect="coverflow" slidesPerView={1} loop className="mySwiper !mx-[-20px] h-[240px] relative">
+      <Swiper
+        effect="coverflow"
+        slidesPerView={1}
+        loop
+        className="mySwiper !mx-[-20px] h-[240px] relative"
+        onSlideChangeTransitionEnd={(event) => handleSlideChange(event.realIndex)}
+      >
         <SwiperSlide className={styles['swiper-slide']}>
           <img src="https://picsum.photos/300/300/?image=100" alt="img1" />
         </SwiperSlide>
         <SwiperSlide className={styles['swiper-slide']}>
           <img src="https://picsum.photos/300/300/?image=1" alt="img2" />
         </SwiperSlide>
-        <div className={styles.tag}>태그</div>
+        <div className={styles.tag}>{currentSlide + 1}/2</div>
       </Swiper>
     </div>
   );
