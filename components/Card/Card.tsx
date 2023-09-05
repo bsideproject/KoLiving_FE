c434671c9220537f523ce222e1571a62bc8be823
JAEMOON <jaemoonzzang@naver.com>
2023-09-05 23:41:06 +0900

    [feat] Room List Like 버튼 및 Mouse Pointer 수정

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -9,9 +9,17 @@ interface CardProps {
 
 export default function Card({ title, content, footer, onClick }: CardProps) {
   return (
-    <div className="w-full border-g3 rounded-[4px] border-[1px]" onClick={onClick}>
-      {title && <div className="py-[8px] px-[12px]">{title}</div>}
-      {content && <div>{content}</div>}
+    <div className="w-full border-g3 rounded-[4px] border-[1px]">
+      {title && (
+        <div className="py-[8px] px-[12px] cursor-pointer" onClick={onClick}>
+          {title}
+        </div>
+      )}
+      {content && (
+        <div className="cursor-pointer" onClick={onClick}>
+          {content}
+        </div>
+      )}
       {footer && <div className="px-[18px]">{footer}</div>}
     </div>
   );
