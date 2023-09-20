acf6b0ae1df11f556caaa593e40825143ccada89
JAEMOONLEE <leejaemoon@lotte.net>
2023-09-20 14:28:09 +0900

    [feat] 룸 등록 step3 우선 커밋...

--- components/File/FileUpload.tsx
+++ components/File/FileUpload.tsx
@@ -34,16 +34,16 @@ export default function FileUpload() {
           dragProps
         }) => (
           // write your building UI
-          <div className="upload__image-wrapper">
-            <div className="relative w-[120px] h-[110px] mt-[8px]" style={isDragging ? { color: "red" } : undefined} {...dragProps}> 
+          <div className="upload__image-wrapper flex items-center w-[600px] overflow-x-auto">
+            <div className={`relative w-[108px] h-[108px] mt-[8px]`} {...dragProps}> 
                 <Rectangle className="z-0" />
-                <RectangleCamera className="absolute z-10 top-[45px] left-[55px] transform -translate-x-1/2 -translate-y-1/2 hover:cursor-pointer" onClick={onImageUpload}/>
-                <span className="absolute z-10 top-[67px] left-[55px] transform -translate-x-1/2 -translate-y-1/2 text-g4 semibold text-[12px]">3/5</span>
+                <RectangleCamera className={`absolute z-10 top-[45px] left-[55px] transform -translate-x-1/2 -translate-y-1/2 hover:cursor-pointer ${isDragging ? "bg-r1": ""}`} onClick={onImageUpload}/>
+                <span className={`absolute z-10 top-[67px] left-[55px] transform -translate-x-1/2 -translate-y-1/2 text-g4 semibold text-[12px] ${isDragging ? "text-r1": ""}`}>{(imageList || []).length}/{maxNumber}</span>
             </div>
             &nbsp;
             {/* <button onClick={onImageRemoveAll}>Remove all images</button> */}
             {imageList.map((image, index) => (
-              <div key={index} className="image-item relative w-[100px] h-[100px] ">
+              <div key={index} className="image-item relative w-[108px] h-[108px] mt-[8px]">
                 <img src={image.dataURL} alt="" className="top-0 left-0 w-full h-full object-cover" />
                 <button 
                   className="absolute top-0 right-0 bg-g5 text-g1 w-5 h-5 -translate-y-[0.5%] cursor-pointer leading-[4px]" 
