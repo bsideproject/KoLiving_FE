3d1f178d23cdd45c6cf2397cad41bc60303f5b75
JAEMOON <jaemoonzzang@naver.com>
2023-09-24 21:17:00 +0900

    [feat] Stepper Component 수정

--- components/Stepper2/Stepper2.tsx
+++ components/Stepper2/Stepper2.tsx
@@ -9,6 +9,9 @@ interface StepperProps {
   callbackCount?: (count: number) => void;
 }
 
+/**
+ * @see Stepper + - Component
+ */
 export default function Stepper2({ disabled, disabledLeft, disabledRight, initCount, callbackCount }: StepperProps) {
   const [count, setCount] = useState(initCount || 0);
 
@@ -16,25 +19,35 @@ export default function Stepper2({ disabled, disabledLeft, disabledRight, initCo
     setCount(initCount);
   }, [initCount]);
 
-  const sendCount = (count: number) => {
-    callbackCount?.(count);
+  const sendCount = (_count: number) => {
+    callbackCount?.(_count);
   };
 
   return (
     <div className={styles.stepper_container}>
       <button
         type="button"
-        className={`${styles.rounded} ${(disabled || disabledLeft) && styles.disabled } `}
-        onClick={() => { setCount(count - 1); sendCount(count-1); return;}}
+        className={`${styles.rounded} ${(disabled || disabledLeft) && styles.disabled} `}
+        onClick={() => {
+          setCount(count - 1);
+          sendCount(count - 1);
+        }}
         disabled={disabled || disabledLeft}
-      >-</button>
-      <span className='w-10 inline-block text-center font-bold text-g7'>{count}</span>
-      <button 
+      >
+        -
+      </button>
+      <span className="w-10 inline-block text-center font-bold text-g7">{count}</span>
+      <button
         type="button"
-        className={`${styles.rounded} ${(disabled || disabledRight) && styles.disabled } `}
-        onClick={() => { setCount(count + 1); sendCount(count+1); return;}}
+        className={`${styles.rounded} ${(disabled || disabledRight) && styles.disabled} `}
+        onClick={() => {
+          setCount(count + 1);
+          sendCount(count + 1);
+        }}
         disabled={disabled || disabledRight}
-      >+</button>
+      >
+        +
+      </button>
     </div>
   );
-};
+}
