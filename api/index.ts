2ff81fd6ec73073926372fa2032693eb6273502c
ywlee <zestlee1106@gmail.com>
2023-09-30 22:18:19 +0900

    [feat] api 호출 방식 수정

--- api/index.ts
+++ api/index.ts
@@ -5,27 +5,16 @@ export const fetchData = async <T>(url: string, options?: RequestInit): Promise<
     const response = await fetch(`${baseURL}${url}`, options);
 
     if (response.status === 204) {
-      // error is: "Type 'null' is not assignable to type 'T'."
       return null;
     }
 
-    if (!response.ok) {
-      throw new Error('Failed to fetch data');
-    }
-
-    // TODO:나중에 삭제
     try {
       const data = await response.json();
       return data;
     } catch (e) {
-      if (response.status === 200) {
-        return null;
-      }
+      throw Error('에러 응답값이 없습니다');
     }
-    const data = await response.json();
-    return data;
   } catch (error) {
-    console.error(error);
-    throw new Error('Failed to fetch data');
+    throw new Error('Failed to fetch data', error as Error);
   }
 };
