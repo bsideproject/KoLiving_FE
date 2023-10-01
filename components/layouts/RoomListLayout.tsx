2910d65b02abedc70da594a30b46255590bf541e
ywlee <zestlee1106@gmail.com>
2023-10-01 19:49:11 +0900

    [feat] room add 버튼 연동 변경 (새로 만든 페이지

--- components/layouts/RoomListLayout.tsx
+++ components/layouts/RoomListLayout.tsx
@@ -1,24 +1,15 @@
 import React, { ReactNode } from 'react';
 import Header from '@/components/Header/Header.tsx';
-import useModal from '@/hooks/useModal.ts';
-import Step1 from '@/pages/room/addRoom/step1.tsx';
+import { useRouter } from 'next/router';
 
 interface AppLayoutProps {
   children: ReactNode;
 }
 
 function RoomListLayout({ children }: AppLayoutProps) {
-  const { openModal } = useModal();
+  const router = useRouter();
   const handleButtonClick = () => {
-    openModal({
-      props: {
-        title: 'Add Rooms',
-        size: 'full',
-        custom: true,
-        customHeader: true,
-      },
-      children: <Step1 />,
-    });
+    router.push('/room/add/step1');
   };
 
   return (
