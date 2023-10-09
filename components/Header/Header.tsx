ef24d7586066e3f0d497e139f1ee14c9699c4e50
JAEMOON <jaemoonzzang@naver.com>
2023-10-09 20:51:41 +0900

    [feat] Room 상세 삭제 버튼 추가

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import LogoBlack from '@/public/icons/logo-black.svg';
 import LogoWhite from '@/public/icons/logo-white.svg';
-// import Plus from '@/public/icons/plus.svg';
 import Plus from '@/public/icons/plus2.svg';
 import Back from '@/public/icons/back.svg';
 import Close from '@/public/icons/close.svg';
 import Pencil from '@/public/icons/pencil.svg';
+import Delete from '@/public/icons/recycleBin.svg';
 import styles from './Header.module.scss';
 import Space from '../Space.tsx';
 
 interface HeaderProps {
   type?: 'logo' | 'back' | 'title';
   title?: string;
-  right?: 'plus' | 'close' | 'pencil';
+  right?: 'plus' | 'close' | 'pencil' | 'delete';
   logoColor?: 'black' | 'white';
   bgColor?: 'white' | 'transparent';
   hasButton?: boolean;
   handleButtonClick?: () => void;
+  handleSecondButtonClick?: () => void;
 }
 
 const LOGOS = {
@@ -32,6 +33,8 @@ const renderHandler = (type: string | undefined, color: string) => {
       return <Close width={24} height={24} className={`${color} stroke-[2]`} />;
     case 'pencil':
       return <Pencil className={`${color} stroke-[2]`} />;
+    case 'delete':
+      return <Delete />;
     default:
       return null;
   }
@@ -44,6 +47,7 @@ export default function Header({
   logoColor = 'black',
   bgColor = 'white',
   handleButtonClick,
+  handleSecondButtonClick,
   hasButton = true,
 }: HeaderProps) {
   const strokeColor = bgColor === 'white' ? 'stroke-g7' : 'stroke-g0';
@@ -74,7 +78,11 @@ export default function Header({
               <Space />
               <div className="font-pretendard font-medium text-[18px]">{title}</div>
               <Space />
-              <button onClick={handleClick}>{renderHandler(right, strokeColor)}</button>
+              {right === 'delete' ? (
+                <Delete onClick={handleSecondButtonClick} />
+              ) : (
+                <button onClick={handleClick}>{renderHandler(right, strokeColor)}</button>
+              )}
             </div>
           </div>
         </div>
