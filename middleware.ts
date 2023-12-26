8bc31bd802efb8463b7b11a191dc0b9b5f96ae66
이영우 <zestlee1106@gmail.com>
2023-12-26 20:31:51 +0900

    [fix] 비로그인 시 액션 막는 것 추가

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-restricted-exports
 export { default } from 'next-auth/middleware';
 
-export const config = { matcher: ['/room/add/:path*', '/userInfo'] };
+export const config = { matcher: ['/room/add/:path*', '/userInfo', '/liked', '/notice'] };
